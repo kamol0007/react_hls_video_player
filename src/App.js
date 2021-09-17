180b5b11c53b6449bf4638ae11d104786365dfa0
kamol0007 <59263172+kamol0007@users.noreply.github.com>
2021-09-17 17:00:05 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, {useState}from 'react';
 import HlsPlayer from './HlsPlayer';
 
 
-function App() {
-    const video_url = "https://prep.uz/video/source/SWcYsjW9WBeJOFAAot46Uw/1631818105/3659/ebkWeZNrE64MNxnBKCYeMF7uKCkCR3UcaRzKv0Uf/enc/playlist.m3u8";
+const App = () => {
+    const [url, setUrl] = useState("https://prep.uz/video/source/i0NjPNJQocD_ytKQjbFlLA/1631902349/3659/ebkWeZNrE64MNxnBKCYeMF7uKCkCR3UcaRzKv0Uf/enc/playlist.m3u8");
+    const [input, setInput] = useState('');
     return (
-		<div style={{width: 700}}>
-			<HlsPlayer video_url={video_url}/>
-		</div>
+        <div style={{width: 700, margin: 'auto', padding: 10}}>
+            <div style={{width: 700}}>
+                <HlsPlayer file={url}/>
+            </div>
+            <div style={{margin: 10}}>
+                <input type="text" name="file" value={input} onChange={e=>setInput(e.target.value)} style={{width: 600}}/>
+                <button onClick={()=>setUrl(input)}>Load</button>
+            </div>
+        </div>
     );
 }
 export default App;
--- src/HlsPlayer.jsx
+++ src/HlsPlayer.jsx
@@ -1,69 +1,78 @@
-import React, { Component } from "react";
+import React, { useEffect, useRef } from 'react';
 import Hls from "hls.js";
 import Plyr from "plyr";
 import 'plyr/dist/plyr.css';
+import Poster from './video-poster.png';
 
 
-class Player extends Component {
-    state = {};
-    componentDidMount() {
-        if (Hls.isSupported() && this.player) {
-            const video = this.player;
-            const source = video.getElementsByTagName("source")[0].src;
-            const defaultOptions = {
-                controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'captions', 'settings',  'airplay', 'fullscreen'],
-                settings: ['quality']
-            };
-            if (Hls.isSupported()) {
-                const hls = new Hls();
-                hls.loadSource(source);
-                hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
-                    const availableQualities = hls.levels.map((l) => l.height)
-                    defaultOptions.quality = {
-                        default: availableQualities[0],
-                        options: availableQualities,
-                        forced: true,        
-                        onChange: (e) => updateQuality(e),
-                    }
-                    const player = new Plyr(video, defaultOptions);
-                });
-                hls.attachMedia(video);
-                window.hls = hls;
-            } else {
-                const player = new Plyr(video, defaultOptions);
-            }
-            function updateQuality(newQuality) {
-                window.hls.levels.forEach((level, levelIndex) => {
-                    if (level.height === newQuality) {
-                        console.log("Found quality match with " + newQuality);
-                        window.hls.currentLevel = levelIndex;
-                    }
-                });
-            }
-        }
-    }
-    render() {
-        return (<>
+const Player = ({ file = '' }) => {
+	const ref = useRef(null);
+	const player = useRef(null);
+	const setPLayer = () => {
+		const src = file;
+		const defaultOptions = {
+			controls: [
+				'play-large',
+				'play',
+				'progress',
+				'current-time',
+				'mute',
+				'volume',
+				'captions',
+				'settings',
+				'airplay',
+				'fullscreen',
+			],
+			settings: ['quality'],
+		};
+		if (Hls.isSupported() && ref.current) {
+			const updateQuality = newQuality => {
+				window.hls.levels.forEach((level, levelIndex) => {
+					if (level.height === newQuality) {
+						window.hls.currentLevel = levelIndex;
+					}
+				});
+			};
+			const hls = new Hls();
+			hls.loadSource(src);
+			hls.on(Hls.Events.MANIFEST_PARSED, (event, data) => {
+				const availableQualities = hls.levels.map(l => l.height);
+				defaultOptions.quality = {
+					default: availableQualities[0],
+					options: availableQualities,
+					forced: true,
+					onChange: e => updateQuality(e),
+				};
+				player.current = new Plyr(ref.current, defaultOptions);
+			});
+			hls.attachMedia(ref.current);
+			window.hls = hls;
+		} else {
+			player.current = new Plyr(ref.current, defaultOptions);
+		}
+	};
+	useEffect(() => {
+		setPLayer();
+	}, [file]);
+	return (
+		<div>
             <style jsx>{`
                 :root {
                     --plyr-color-main: #5254f1!important;
                 }
             `}</style>
-            <video
-                preload="none"
-                className="videoCanvas"
-                ref={player => (this.player = player)}
-                width="100%"
-                height="auto"
-                controls crossorigin playsinline 
-            >
-                <source 
-                    type="application/x-mpegURL" 
-                    src={this.props.video_url}
-                >
-                </source>
-            </video>
-        </>);
-    }
-}
+			<video
+				preload="none"
+				className="videoCanvas"
+				ref={ref}
+				width="100%"
+				height="auto"
+				controls
+				src={file}
+				poster={Poster}
+				crossOrigin
+				playsInline/>
+		</div>
+	);
+};
 export default Player;
